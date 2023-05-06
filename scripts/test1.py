6b3054736aa306bdd4ee924317bb8a98184b8a7b
shawn-phy <philipoyuga14@gmail.com>
2023-05-06 23:49:34 +0300

    fixed webdriver issue

--- scripts/test1.py
+++ scripts/test1.py
@@ -1,12 +1,19 @@
+from helium import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
 
+driver = webdriver.chrome()
 
-PATH = r"C:\Program Files (x86)\chromedriver.exe"
+start_chrome('https://users.verbit.co/')
+write('michaeldamian935@gmail.com', into='email')
+click('next')
+write('Alfie9080*', into = 'password')
+click('login')
 
+for i in range(10): # Refresh the page 10 times
+	webdriver.implicitly_wait(2)
+	webdriver.refresh()
 
-driver = webdriver.Chrome()
-
-driver.get('https://users.verbit.co/')
 
